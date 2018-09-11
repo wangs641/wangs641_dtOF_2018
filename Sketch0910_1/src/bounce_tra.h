b97aea9cc3765c23dd8297b368c3034244df5a7c
wangs641 <40814606+wangs641@users.noreply.github.com>
2018-09-11 17:05:02 -0400

    homework of shuyang

---
+++ Sketch0910_1/src/bounce_tra.h
@@ -0,0 +1,22 @@
+#pragma once
+#include "ofMain.h"
+
+class Bounce_tra {
+public:
+    
+    void setup (float posX,  float posY, float _r);
+    void update();
+    void draw();
+    
+    float x, y, r;
+    ofColor color;
+    float dirX, dirY;
+
+    
+    
+    
+    
+    
+    
+    
+};
